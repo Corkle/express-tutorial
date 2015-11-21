deece7232f44b4de73e18d4d2add2e011fbc6119
Corey McDaniel <corey.r.mcdaniel@live.com>
2015-11-20 16:11:04 -0800

    Added Passport Auth config

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var swig = require('swig');
-var bodyParser = require('body-parser');
+var express = require('express'),
+	swig = require('swig'),
+	bodyParser = require('body-parser'),
+	passport = require('passport');
 
 module.exports = function() {
 	var app = express();
@@ -14,6 +15,9 @@ module.exports = function() {
 	app.engine('html', swig.renderFile);
 	app.set('views', './app/views');
 	app.set('view engine', 'html');
+	
+	app.use(passport.initialize());
+	app.use(passport.session());
 	
 	require('../app/routes/index.server.routes.js')(app);
 	require('../app/routes/users.server.routes.js')(app);
